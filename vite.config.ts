c8cae70e721c423c365e9ff2d607293cb92a8e48
kjsu <surtne@gmail.com>
2025-10-21 01:27:30 +0900

    feat: GitHub Pages 배포를 위한 설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 
 export default defineConfig({
+  base: "/drt-solo-demo/",
   plugins: [react()],
   css: {
     postcss: './postcss.config.js',
