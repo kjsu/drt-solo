f8f149659ef35a912aca80c49d33d1e3f8bdc9b2
kjsu <surtne@gmail.com>
2025-10-21 01:36:00 +0900

    fix: base 수정

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 
 export default defineConfig({
-  base: "/drt-solo-demo/",
+  base: "/drt-solo/",
   plugins: [react()],
   css: {
     postcss: './postcss.config.js',
